ee1bb39d5c174a712182ab55787b6360c1538ae2
GuilhRib21 <ribeiro.guilhermedesouza@gmail.com>
2021-11-24 12:55:24 -0300

    Firts Project

---
+++ ProjectList/BankProject/Enum/TipoConta.cs
@@ -0,0 +1,8 @@
+namespace BankProject
+{
+    public enum TipoConta
+    {
+        PessoaFisica = 1,
+        PessoaJuridica = 2
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Public Projects - GuilhRib21 :sunglasses:
+
+1. Bank Project - Simulation of bank actions on .net
